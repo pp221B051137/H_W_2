224e9dba090c4b53c3c99c2a0da92a831abe4d89
pp221B051137 <98762796+pp221B051137@users.noreply.github.com>
2022-09-22 19:58:12 +0300

    Delete Thursday directory

--- Thursday/4.py
+++
@@ -1,9 +0,0 @@
-import re
-password = list(input().split(','))
-empty = []
-for w in password:
-    x = re.findall("[a-zA-Z]",w)
-    y = re.findall("[0-9]",w)
-    z = re.findall("[$#@]",w)
-    if x != empty and y!= empty and z!= empty and len(w) >= 6 and len(w) <=12:
-        print(w)
